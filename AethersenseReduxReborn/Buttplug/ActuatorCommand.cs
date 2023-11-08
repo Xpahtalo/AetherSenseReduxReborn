b9199e53ec95e7eef9405a24a874b11b42d6d54a
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-11-07 16:45:51 -0800

    simplify and change display

--- AethersenseReduxReborn/Buttplug/ActuatorCommand.cs
+++ AethersenseReduxReborn/Buttplug/ActuatorCommand.cs
@@ -7,13 +7,12 @@ public readonly record struct ActuatorCommand
         get => _value;
         private init => _value = double.Clamp(value, 0, 1);
     }
-    public int Display => (int)(Value * 100);
 
     public ActuatorCommand(double value) { Value = value; }
 
     public ActuatorCommand Quantized(uint steps) => new(Quantize(Value, steps));
 
-    public override string ToString() => Display.ToString();
+    public override string ToString() => Value.ToString("F2");
 
-    public static double Quantize(double value, uint steps) => double.Round(steps * double.Clamp(value, 0, 1)) / steps;
+    private static double Quantize(double value, uint steps) => double.Round(steps * double.Clamp(value, 0, 1)) / steps;
 }
